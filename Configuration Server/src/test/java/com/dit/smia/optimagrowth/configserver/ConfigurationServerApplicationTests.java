5d8a50bdfd4aa7602490fe2b7b7af79015b56026
Daniel Xu <zhiqiang.xu2015@gmail.com>
2022-11-27 23:50:57 -0500

    Building our Spring Cloud Configuration Server

---
+++ Configuration Server/src/test/java/com/dit/smia/optimagrowth/configserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dit.smia.optimagrowth.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
